caf0db3ccb9f12e9b1322ee492700e94fb8524e8
Arden Diago <diagoarden@gmail.com>
2024-05-15 15:27:24 +0530

    Change App Info

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import AboutUs from "./components/HomePage/AboutUs/AboutUs";
 import Header from "./components/HomePage/Header/Header";
 import Background from "./components/HomePage/Background/Background";
-
+import Test from "./components/Test";
 function App() {
   return (
     <>
-      <Background />
-      {/* <Header /> */}
-      {/* <AboutUs /> */}
+      {/* <Background /> */}
+      <Header />
+      <AboutUs />
+      <Test />
     </>
   );
 }
