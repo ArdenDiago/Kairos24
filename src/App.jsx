e370ae37d4ffd0406627d84dbb011a113f817834
Arden Diago <diagoarden@gmail.com>
2024-05-24 11:27:42 +0530

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Header from "./components/Home/Header/Header";
 import Events from "./components/Events/Events";
-// import AboutUs from "./components/AboutUs/AboutUs";
+import AboutUs from "./components/AboutUs/AboutUs";
 
 function App() {
   return (
     <>
     <Header />
     <Events />
-    {/* <AboutUs /> */}
+    <AboutUs />
     </>
   );
 }
