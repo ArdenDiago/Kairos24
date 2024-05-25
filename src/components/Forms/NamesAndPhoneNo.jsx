f250ee0ef6a699b67cf1c5b17c46d8ebf95d60ec
Arden Diago <diagoarden@gmail.com>
2024-05-25 13:53:03 +0530

    form css

--- src/components/Forms/NamesAndPhoneNo.jsx
+++ src/components/Forms/NamesAndPhoneNo.jsx
@@ -1,8 +1,8 @@
-export default function NamesAndPhoneNo({text, textType, textId, textPlaceHolder, onValueChange}) {
+export default function NamesAndPhoneNo({text, textType, textId, textPlaceHolder, onValueChange, ability}) {
   return (
     <>
       <label>{text}</label>
-      <input type={textType} id={textId} placeholder={textPlaceHolder} onChange={onValueChange}/>
+      <input type={textType} id={textId} placeholder={textPlaceHolder} onChange={onValueChange} required disabled={ability}/>
     </>
   );
 }
