e069df335e776ff4ec7552c342fb3b75f4bae657
Jetandar N Silwani <jeetandar.n.silwani@gmail.com>
2024-05-25 21:09:36 +0530

    jns

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Home/Header/Header";
-// import Events from "./components/Events/Events";
+//import Events from "./components/Events/Events";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Forms from "./components/Forms/Forms";
 
