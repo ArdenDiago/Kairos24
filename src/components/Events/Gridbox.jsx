cfe5bd34d7a9cde3ed1f4fb61396a162735d19eb
Arden Diago <diagoarden@gmail.com>
2024-05-27 18:02:37 +0530

    final

--- src/components/Events/Gridbox.jsx
+++ src/components/Events/Gridbox.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function Gridbox({
   eventImg,
   text,
@@ -12,20 +14,28 @@ export default function Gridbox({
       <img className="card-img-top" src={eventImg} alt="Card image cap" />
       <div className="card-body">
         <h1>{eventName}</h1>
-        <div className="time-venus">
+        <div className="time-venu">
           <div>{time}</div>
           <div>{venu}</div>
         </div>
-        <h3>Registration AMT: {regAMT}</h3>
-        <p className="card-text">{text}</p>
+        <ul className="card-text" dangerouslySetInnerHTML={{__html: text}}></ul>
+        <h3>Registration Fees: {regAMT}</h3>
         <div className="card-contact">
           <div className="card-contact-left">
-            <div>Name {codName[0].name}</div>
-            <div>+91 {codName[0].number}</div>
+            {codName[0] && (
+              <>
+                <div>Name: {codName[0].name}</div>
+                <div>+91 {codName[0].number}</div>
+              </>
+            )}
           </div>
-          <div>
-            <div>Name {codName[1].name}</div>
-            <div>+91 {codName[1].number}</div>
+          <div className="card-contact-right">
+            {codName[1] && (
+              <>
+                <div>Name: {codName[1].name}</div>
+                <div>+91 {codName[1].number}</div>
+              </>
+            )}
           </div>
         </div>
       </div>
