ccca2f33187197574fabc9711b359ac2c849693b
Arden Diago <diagoarden@gmail.com>
2024-05-28 09:51:17 +0530

    a

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -6,9 +6,7 @@ import Button from "../Buttons/Buttons";
 import Text from "../Text/Text";
 
 export default function Header() {
-  function eventClicked(id) {
-    
-  }
+  function eventClicked(id) {}
   return (
     <section id="Home" className="hero-section">
       <div className="top-10">
@@ -18,8 +16,12 @@ export default function Header() {
         <Text />
       </div>
       <div className="btn-master">
-          <Button btnTarget='_blank' clickHandler='https://drive.google.com/file/d/1SUgLUW10s4OFs89hcw16qaFM_JAu8RlR/view' text="Brochure" />
-          <Button btnTarget='_self' clickHandler={'#Reg'} text="Register Now" />
+        <Button
+          btnTarget="_blank"
+          clickHandler="https://drive.google.com/file/d/1SUgLUW10s4OFs89hcw16qaFM_JAu8RlR/view"
+          text="Brochure"
+        />
+        <Button btnTarget="_self" clickHandler={"#Reg"} text="Register Now" />
       </div>
     </section>
   );
