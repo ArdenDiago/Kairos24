3849e14223bb95a2b88e7232465d4d0f62678ae9
Arden Diago <diagoarden@gmail.com>
2024-05-30 11:54:19 +0530

    Our Team

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AboutUs from "./components/AboutUs/AboutUs";
 import Forms from "./components/Forms/Forms";
 import Loader from "./components/Loader.jsx/Loader";
 import CardEvents from "./components/Events/CardEvents.jsx";
+import OurTeam from "./components/OurTeam/OurTeam.jsx";
 import { Suspense } from "react";
 
 function App() {
@@ -10,10 +11,11 @@ function App() {
   return (
     <>
       <Suspense fallback={<Loader />}>
-        <Header />
-        <CardEvents />
-        <Forms />
-        <AboutUs />
+        <OurTeam />
+        {/* <Header /> */}
+        {/* <CardEvents /> */}
+        {/* <Forms /> */}
+        {/* <AboutUs /> */}
       </Suspense>
     </>
   );
