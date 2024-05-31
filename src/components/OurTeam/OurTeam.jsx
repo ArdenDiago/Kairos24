09575389670ee69b00caf1903c76e5113460a812
Arden Diago <diagoarden@gmail.com>
2024-05-31 13:12:06 +0530

    d

--- src/components/OurTeam/OurTeam.jsx
+++ src/components/OurTeam/OurTeam.jsx
@@ -2,7 +2,9 @@ import "./OurTeam.css";
 
 import CardTeam from "./CardTeam";
 
-const imgset = ["https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft12.svg"]
+const imgset = [
+  "https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ft12.svg",
+];
 
 export default function OurTeam() {
   return (
@@ -11,7 +13,11 @@ export default function OurTeam() {
         <p class="text-blk team-head-text">Our Team</p>
 
         <div class="responsive-container-block teamcard-container pb-10">
-          <CardTeam imageFile={imgset} designation="Project Manager" name="James Bond" />
+          <CardTeam
+            imageFile={imgset}
+            designation="Project Manager"
+            name="James Bond"
+          />
         </div>
       </div>
     </>
